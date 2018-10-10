e8bc92f2638bc891eda2b51f8e0cf89835baee1a
Robert Arbon <robert.arbon@bristol.ac.uk>
2018-10-10 12:46:56 +0100

    Added download data script and small format change for source.txt

---
+++ download_data.R
@@ -0,0 +1,8 @@
+library(data.table)
+file_name <- 'dili_from_anti_TB_treatment.csv'
+base_url <- 'http://data.bris.ac.uk/datasets/1vdt21e4mhxxd27hso89cqmhhh/'
+local_dir <- 'data/from_web/'
+local_path <- paste0(local_dir, file_name)
+file_url <- paste0(base_url, file_name)
+df <- fread(file_url)
+save(df, file=local_path)
