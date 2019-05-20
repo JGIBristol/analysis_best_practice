d7e06c01662863480a7a7e58acb019c165694c30
Robert Arbon <robert.arbon@bristol.ac.uk>
2019-05-20 09:17:14 +0100

    deleted unneeded files

--- scripts/download_data.R
+++
@@ -1,8 +0,0 @@
-library(data.table)
-file_name <- 'dili_from_anti_TB_treatment.csv'
-base_url <- 'http://data.bris.ac.uk/datasets/1vdt21e4mhxxd27hso89cqmhhh/'
-local_dir <- 'data/original/'
-local_path <- paste0(local_dir, file_name)
-file_url <- paste0(base_url, file_name)
-df <- fread(file_url)
-save(df, file=local_path)
